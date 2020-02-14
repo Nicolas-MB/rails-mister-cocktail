7d9a76b3561d0d695679fde5d1ae25227043b3e8
Nicolas Martin-Bouyer <nicolas.martinbouyer@gmail.com>
2020-02-14 11:49:36 +0100

    added root to

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,35 @@
+class DosesController < ApplicationController
+
+  def new
+  @dose = Dose.new
+  end
+
+  def create
+  @dose = Dose.new(params_dose)
+  @cocktail = Cocktail.find(params[:cocktail_id])
+  @dose.cocktail = @cocktail
+       if @dose.save
+        redirect_to cocktail_path(@cocktail)
+       else
+        render "cocktails/show"
+
+      end
+  end
+
+  def destroy
+    @dose = Dose.find(params[:id])
+    @dose.cocktail
+
+    @dose.destroy
+    redirect_to cocktail_path(@dose.cocktail)
+
+  end
+
+private
+
+def params_dose
+  params.require(:dose).permit(:description, :ingredient_id)
+end
+
+
+end
